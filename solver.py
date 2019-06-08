7cb0007291d5cf28217e918500f61c2e63134d72
Niels van Huijstee <niels@huijs.net>
2019-06-08 14:27:24 +0200

    Basic filling.

---
+++ solver.py
@@ -0,0 +1,63 @@
+def complete_zeros(line):
+    if line.count(1) != len(line) / 2:
+        return
+    for position, value in enumerate(line):
+        if value is not None:
+            continue
+        line[position] = 0
+
+
+def complete_ones(line):
+    if line.count(0) != len(line) / 2:
+        return
+    for position, value in enumerate(line):
+        if value is not None:
+            continue
+        line[position] = 1
+
+
+def fill_between(line):
+    for position, value in enumerate(line):
+        fill_position = position + 1
+        compare_position = position + 2
+        if value is None:
+            continue
+        if line[fill_position] is not None:
+            continue
+        if compare_position >= len(line):
+            continue
+        if line[position] != line[compare_position]:
+            continue
+        line[fill_position] = (value + 1) % 2
+
+
+def fill_left_of_double(line):
+    for position, value in enumerate(line):
+        fill_position = position - 1
+        compare_position = position + 1
+
+        if value is None:
+            continue
+        if fill_position < 0:
+            continue
+        if compare_position >= len(line):
+            continue
+        if line[position] != line[compare_position]:
+            continue
+        line[fill_position] = (value + 1) % 2
+
+
+def fill_right_of_double(line):
+    for position, value in enumerate(line):
+        fill_position = position + 2
+        compare_position = position + 1
+
+        if value is None:
+            continue
+        # if fill_position >= len(line):
+        #     continue
+        if compare_position >= len(line):
+            continue
+        if line[position] != line[compare_position]:
+            continue
+        line[fill_position] = (value + 1) % 2
---
+++ test_solver.py
@@ -0,0 +1,57 @@
+from solver import (
+    complete_ones,
+    complete_zeros,
+    fill_between,
+    fill_left_of_double,
+    fill_right_of_double,
+)
+
+
+_ = None
+
+# grid_8 = [
+#     [0, _, _, 0, _, 1, _, _],
+#     [_, _, 1, _, _, _, _, 1],
+#     [_, _, _, _, 0, _, 0, _],
+#     [_, _, _, _, _, _, _, _],
+#     [_, _, _, _, _, _, _, _],
+#     [_, _, _, _, 0, 0, _, _],
+#     [0, _, _, _, 1, _, 1, _],
+#     [_, 1, _, 1, _, _, _, 1],
+# ]
+
+
+def test_complete_ones():
+    line = [_, _, 0, 0, _, 0, 1, 0]
+    complete_ones(line)
+    assert [1, 1, 0, 0, 1, 0, 1, 0] == line
+
+
+def test_complete_zeros():
+    line = [_, 1, _, 1, _, 0, 1, 1]
+    complete_zeros(line)
+    assert [0, 1, 0, 1, 0, 0, 1, 1] == line
+
+
+def test_fill_ones_between_zeros():
+    line = [_, _, _, _, 0, _, 0, _]
+    fill_between(line)
+    assert [_, _, _, _, 0, 1, 0, _] == line
+
+
+def test_fill_zeros_between_ones():
+    line = [1, _, 1, _, 1, 0, 1, _]
+    fill_between(line)
+    assert [1, 0, 1, 0, 1, 0, 1, _] == line
+
+
+def test_fill_left_of_double():
+    line = [_, 0, 0, _, _, _, 0, 0]
+    fill_left_of_double(line)
+    assert [1, 0, 0, _, _, 1, 0, 0] == line
+
+
+def test_fill_right_of_double():
+    line = [0, 0, _, _, _, 0, 0, _]
+    fill_right_of_double(line)
+    assert [0, 0, 1, _, _, 0, 0, 1] == line
